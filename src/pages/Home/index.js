8f957c23679d17a00fcf7275c1234d37a949ecd4
James Rovel Barbosa <diagjames@gmail.com>
2021-04-19 20:32:22 -0300

    more

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,16 @@
+import munster from '../images/munster.png';
+
 
 export default function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <h1>You may like</h1>
+            <div className="posts">
+                <div box-one>
+                    <h2>Munster frequency</h2>
+                    <img src= {munster} />
+                </div>
+            </div>
         </div>
-        );
+    );
 }
